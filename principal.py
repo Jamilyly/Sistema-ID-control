f522922882347504ab7ef9147f2f80fb6411a67c
Jamilyly <jamilyemanuelle102@gmail.com>
2024-09-18 10:27:34 -0400

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,24 @@
+from Volei import cbv_meninas
+carta1=cbv_meninas("Ana Carolina",32,73,1.83,19,29)
+##Segunda jogadora
+carta2=cbv_meninas("Fernanda Garay",37,74,1.79,24,21)
+
+#Terceira Jogador
+carta3=cbv_meninas("Carol Gattaz",42,78,1.93,21,20)
+
+##Segunda jogadora
+carta4=cbv_meninas("Camila Brait",36,59,1.70,17,25)
+
+##Quarta jogadora
+carta5=cbv_meninas("Thaisa Dahner",36,87,1.96,23,35)
+
+##Quinta jogadora
+carta6=cbv_meninas("Marcris Carneiro",35,60,1.78,26,16)
+
+##Sexta Jogadora]
+carta7=cbv_meninas("Gabriela Guimarães",30,65,1.80,11,30)
+
+##Sétima jogadora
+carta8=cbv_meninas("Natália Pereira",34,76,1.86,22,27)
+
+         
